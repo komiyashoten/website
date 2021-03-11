09c22e50bafa43c96ba814aa3f70132dbc23c9b2
okapiMacBookPro16 <okapi3@mac.com>
2021-03-11 18:06:12 +0900

    カスタムフィールドプラグイン追加

--- wp-content/themes/komiya/lib/custom-post-type.php
+++ wp-content/themes/komiya/lib/custom-post-type.php
@@ -1,38 +1,38 @@
 <?php
 
-// add_action( 'init', 'create_post_type' );
-// function create_post_type() {
-// 	register_post_type( 'topFeature',
-//     array(
-//       'labels' => array(
-//         'name' => __( 'TOPコンテンツ' ),
-//         'singular_name' => __( 'topFeature' )
-//       ),
-// 		'public' => true,
-// 		'publicly_queryable' => true,
-// 		'show_ui' => true,
-// 		'query_var' => true,
-// 		'rewrite' => true,
-// 		'capability_type' => 'post',
-// 		'hierarchical' => false,
-// 		'menu_position' => 4,
-// 		'supports' => array('title','editor','thumbnail','custom-fields','excerpt','author','trackbacks','comments','revisions','page-attributes'),
-// 		'has_archive' => true
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	register_post_type( 'topFeature',
+    array(
+      'labels' => array(
+        'name' => __( 'TOPページ' ),
+        'singular_name' => __( 'topFeature' )
+      ),
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => 4,
+		'supports' => array('title','editor','thumbnail','custom-fields','excerpt','author','trackbacks','comments','revisions','page-attributes'),
+		'has_archive' => true
 
-//     )
-//   );
-// 	register_taxonomy(
-// 		'topFeature-cat', 
-// 		'topFeature', 
-// 		array(
-// 		  'hierarchical' => true, 
-// 		  'update_count_callback' => '_update_post_term_count',
-// 		  'label' => 'カテゴリー',
-// 		  'singular_label' => 'カテゴリー',
-// 		  'public' => true,
-// 		  'show_ui' => true
-// 		)
-// 	);
+    )
+  );
+	register_taxonomy(
+		'topFeature-cat', 
+		'topFeature', 
+		array(
+		  'hierarchical' => true, 
+		  'update_count_callback' => '_update_post_term_count',
+		  'label' => 'カテゴリー',
+		  'singular_label' => 'カテゴリー',
+		  'public' => true,
+		  'show_ui' => true
+		)
+	);
 	
-// 	flush_rewrite_rules( false );
-// }
+	flush_rewrite_rules( false );
+}
