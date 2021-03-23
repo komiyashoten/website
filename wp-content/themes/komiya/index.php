d87dd86b952c34a443b8b8c2f18c2f31a0662814
折田晋哉 <creative@shinyaorita.com>
2021-03-23 13:16:39 +0900

    ヘッダー営業時間帯修正

--- wp-content/themes/komiya/index.php
+++ wp-content/themes/komiya/index.php
@@ -1,6 +1,6 @@
 <?php get_header('top'); ?>
  
-<div style="padding: 0.3em 1em 1em 1em;"><a href="https://www.komiyakasa.jp/rinji-news/" style="font-size: 1.3rem; padding: 0.8em; background: #efe8e1; width: 100%; display: block; color: #333; text-align: center;"><span style="color: #af8569;">●</span> 新型コロナ対策のため、営業日時を変更しています。 ※オンラインショップは通常営業 [12/22更新]</a></div>
+<div style="padding: 0.3em 1em 1em 1em;"><a href="https://www.komiyakasa.jp/2021-4/" style="font-size: 1.3rem; padding: 0.8em; background: #efe8e1; width: 100%; display: block; color: #333; text-align: center;"><span style="color: #af8569;">●</span> 「東日本橋ショップ」の営業時間変更につきまして（4月より毎週土曜日営業いたします。） [3/8更新]</a></div>
 
 <section>
 
