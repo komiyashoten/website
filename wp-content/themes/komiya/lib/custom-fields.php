87a51aec8589b1d12a272bc4ff827398aa1089ce
okapiMacBookPro16 <okapi3@mac.com>
2021-03-30 09:35:10 +0900

    再度カスタムフィールド修正

--- wp-content/themes/komiya/lib/custom-fields.php
+++ wp-content/themes/komiya/lib/custom-fields.php
@@ -8,6 +8,23 @@ function add_custom_field() {
     add_meta_box( 'goodsURLs', '商品URL', 'create_goodsurls', 'product', 'normal' );
 }
  
+function create_summary() {
+    $keyname = array('まとめ英語タイトル', 'まとめ日本語タイトル');
+    global $post;
+    foreach($keyname as $key):
+        if($key == 'まとめ英語タイトル'):
+            $placeholder = 'まとめタイトル（英語）';
+        else:
+            $placeholder = 'まとめタイトル（日本語）';
+        endif;
+        // 保存されているカスタムフィールドの値を取得
+        $get_value = get_post_meta( $post->ID, $key, true );
+        // nonceの追加
+        wp_nonce_field( 'action-' . $key, 'nonce-' . $key );
+        // HTMLの出力
+        echo '<input name="' . $key . '" value="' . $get_value . '" style="width:99%; border-radius: 3px; border: solid 1px #DEDEDE; padding: 5px; margin-bottom: 10px;" placeholder="'.$placeholder.'">';
+    endforeach;
+}
 
 function create_goodsurls() {
     $keyname = array('楽天', 'Yahoo', 'Amazon');
